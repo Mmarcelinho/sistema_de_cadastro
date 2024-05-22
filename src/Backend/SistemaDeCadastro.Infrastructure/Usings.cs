febe109b8c2c7ab9ce3194358142d38cff525fd0
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-22 15:29:09 -0300

    Adiciona FluentMigrator e configura serviço com SqlServer

--- src/Backend/SistemaDeCadastro.Infrastructure/Usings.cs
+++ src/Backend/SistemaDeCadastro.Infrastructure/Usings.cs
@@ -1,2 +1,8 @@
 global using Microsoft.Data.SqlClient;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using Dapper;
+global using System.Reflection;
+global using FluentMigrator.Runner;
+global using SistemaDeCadastro.Domain.Extension;
+
