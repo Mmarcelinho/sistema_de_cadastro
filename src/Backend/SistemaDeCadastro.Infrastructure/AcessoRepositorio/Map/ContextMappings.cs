cb04895903d054bdc7976b20b6969d7d1f5dfffd
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-22 20:35:06 -0300

    Adiciona classe estática para recuperar tabelas das entidades

---
+++ src/Backend/SistemaDeCadastro.Infrastructure/AcessoRepositorio/Map/ContextMappings.cs
@@ -0,0 +1,6 @@
+namespace SistemaDeCadastro.Infrastructure.AcessoRepositorio.Map;
+
+    public static class ContextMappings
+    {
+        public static string RecuperarTabelaCadastro() => "Cadastros";
+    }
