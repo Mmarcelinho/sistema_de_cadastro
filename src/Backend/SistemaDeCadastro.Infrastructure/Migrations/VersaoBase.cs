5564213920e91f1f17f23bacf01f7b1ac40ab5b5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-22 15:42:05 -0300

    Adiciona versão base

---
+++ src/Backend/SistemaDeCadastro.Infrastructure/Migrations/VersaoBase.cs
@@ -0,0 +1,11 @@
+namespace SistemaDeCadastro.Infrastructure.Migrations;
+
+public class VersaoBase
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InserirColunasPadrao(ICreateTableWithColumnOrSchemaOrDescriptionSyntax tabela)
+    {
+        return tabela
+        .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+        .WithColumn("DataCriacao").AsDateTime().NotNullable();
+    }
+}
