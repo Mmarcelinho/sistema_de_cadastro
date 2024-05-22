436bda591e3f7bfed6f909cd6eafde9083a69179
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-22 14:27:21 -0300

    Adiciona método estático para criar base de dados

---
+++ src/Backend/SistemaDeCadastro.Infrastructure/Migrations/Database.cs
@@ -0,0 +1,17 @@
+namespace SistemaDeCadastro.Infrastructure.Migrations;
+
+public static class Database
+{
+    public static void CriarDatabase(string conexaoComBancoDeDados, string nomeDatabase)
+    {
+        using var minhaConexao = new SqlConnection(conexaoComBancoDeDados);
+
+        var parametros = new DynamicParameters();
+        parametros.Add("nome", nomeDatabase);
+
+        var registros = minhaConexao.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @nome", parametros);
+
+        if (!registros.Any())
+            minhaConexao.Execute($"CREATE DATABASE {nomeDatabase}");
+    }
+}
