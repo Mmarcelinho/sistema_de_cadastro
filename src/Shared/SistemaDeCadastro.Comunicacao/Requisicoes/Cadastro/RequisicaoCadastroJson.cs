61b90244d9759a7e7f49e84f0e6d9a3b4543502a
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-24 18:26:26 -0300

    Adicionado dto de requisição da entidade Cadastro

---
+++ src/Shared/SistemaDeCadastro.Comunicacao/Requisicoes/Cadastro/RequisicaoCadastroJson.cs
@@ -0,0 +1,23 @@
+namespace SistemaDeCadastro.Comunicacao.Requisicoes.Cadastro;
+
+public record RequisicaoCadastroJson(
+    string NomeFantasia,
+    string SobrenomeSocial,
+    bool Empresa,
+    bool CredencialBloqueada,
+    string CredencialExpirada,
+    string CredencialSenha,
+    bool InscritoAssinante,
+    bool InscritoAssociado,
+    string InscritoSenha,
+    bool ParceiroCliente,
+    bool ParceiroFornecedor,
+    bool ParceiroPrestador,
+    bool ParceiroColaborador,
+    string DocumentoNumero,
+    string DocumentoOrgaoEmissor,
+    string DocumentoEstadoEmissor,
+    DateTime DocumentoDataValidade,
+    int IdentificacaoEmpresa,
+    string IdentificacaoIdentificador,
+    short IdentificacaoTipo);
