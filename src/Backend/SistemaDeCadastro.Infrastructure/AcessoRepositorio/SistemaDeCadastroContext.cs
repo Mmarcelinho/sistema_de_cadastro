f31332c5380d9112063d3fa6d8442310c4ea280a
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-29 23:48:05 -0300

    Adiciona context

---
+++ src/Backend/SistemaDeCadastro.Infrastructure/AcessoRepositorio/SistemaDeCadastroContext.cs
@@ -0,0 +1,13 @@
+namespace SistemaDeCadastro.Infrastructure.AcessoRepositorio;
+
+    public class SistemaDeCadastroContext : DbContext
+    {
+        public SistemaDeCadastroContext(DbContextOptions<SistemaDeCadastroContext> options) : base(options) { }
+
+        public DbSet<Cadastro> Cadastros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SistemaDeCadastroContext).Assembly);
+    }
+    }
