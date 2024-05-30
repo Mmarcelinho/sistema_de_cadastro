886319f5d7c9fc25cd40bb3c6e715c995712f0e8
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-29 22:07:18 -0300

    Adiciona mensagens de erro das entidades Cadastro e Pessoa

---
+++ src/Backend/SistemaDeCadastro.Exceptions/PessoaMensagensDeErro.cs
@@ -0,0 +1,19 @@
+namespace SistemaDeCadastro.Exceptions;
+
+public static class PessoaMensagensDeErro
+{
+    public static string PESSOA_CPF_EMBRANCO = "O CPF deve ser informado.";
+
+    public static string PESSOA_CPF_INVALIDO = "O CPF informado é inválido.";
+
+    public static string PESSOA_CNPJ_INVALIDO = "O CNPJ informado é inválido.";
+
+    public static string PESSOA_NOME_EMBRANCO = "O nome deve ser informado.";
+
+    public static string PESSOA_NASCIMENTO_INVALIDO = "A data de nascimento é inválida.";
+
+    public static string PESSOA_TOKEN_EMBRANCO = "O token deve ser informado.";
+
+    public static string PESSOA_NAO_ENCONTRADA = "Pessoa não encontrada.";
+
+}
