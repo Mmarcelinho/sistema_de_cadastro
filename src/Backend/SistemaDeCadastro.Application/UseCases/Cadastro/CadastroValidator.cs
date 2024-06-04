b93ed27480f5a39114a6f122d2286ea0e3005ed0
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-04 14:25:34 -0300

    Adiciona validator de Cadastro e do caso de uso Registrar

---
+++ src/Backend/SistemaDeCadastro.Application/UseCases/Cadastro/CadastroValidator.cs
@@ -0,0 +1,32 @@
+namespace SistemaDeCadastro.Application.UseCases.Cadastro;
+
+public class CadastroValidator : AbstractValidator<RequisicaoCadastroJson>
+{
+    public CadastroValidator()
+    {
+        RuleFor(x => x.Email)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_EMAIL_EMBRANCO)
+        .EmailAddress().WithMessage(CadastroMensagensDeErro.CADASTRO_EMAIL_INVALIDO);
+
+        RuleFor(x => x.NomeFantasia)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_NOME_FANTASIA_EMBRANCO);
+
+        RuleFor(x => x.CredencialSenha)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_CREDENCIAL_SENHA_EMBRANCO);
+
+        RuleFor(x => x.DocumentoNumero)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_DOCUMENTO_NUMERO_EMBRANCO);
+
+        RuleFor(x => x.DocumentoOrgaoEmissor)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_DOCUMENTO_ORGAO_EMISSOR_EMBRANCO);
+
+        RuleFor(x => x.DocumentoDataValidade)
+        .GreaterThan(DateTime.MinValue).WithMessage(CadastroMensagensDeErro.CADASTRO_DOCUMENTO_DATA_VALIDADE_INVALIDA);
+
+        RuleFor(x => x.IdentificacaoIdentificador)
+        .NotEmpty().WithMessage(CadastroMensagensDeErro.CADASTRO_IDENTIFICADOR_EMBRANCO);
+
+        RuleFor(x => x.Empresa)
+        .NotNull().WithMessage(CadastroMensagensDeErro.CADASTRO_EMPRESA_INVALIDA);
+    }
+}
