770796472ed8d6cc43e5fc71f1aa5b12922174d5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-04 14:27:13 -0300

    Adiciona global usings e alterações no código

---
+++ src/Backend/SistemaDeCadastro.Application/Usings.cs
@@ -0,0 +1,14 @@
+global using FluentValidation;
+global using SistemaDeCadastro.Comunicacao.Requisicoes.Cadastro;
+global using SistemaDeCadastro.Exceptions;
+global using SistemaDeCadastro.Comunicacao.Respostas.Cadastro;
+
+
+global using SistemaDeCadastro.Domain.Entidades;
+global using SistemaDeCadastro.Domain.Enum;
+global using SistemaDeCadastro.Domain.Repositorios;
+global using SistemaDeCadastro.Domain.Repositorios.Cadastro;
+global using SistemaDeCadastro.Domain.ValueObjects;
+global using SistemaDeCadastro.Exceptions.ExceptionsBase;
+
+
