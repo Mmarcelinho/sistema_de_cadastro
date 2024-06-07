d8c0839c723e1359c595fe427b62604561709f94
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-07 20:47:57 -0300

    Altera pacotes e gera migrations

--- src/Backend/SistemaDeCadastro.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/SistemaDeCadastro.Infrastructure/DependencyInjectionExtension.cs
@@ -1,43 +1,20 @@
-
-
 namespace SistemaDeCadastro.Infrastructure;
 
 public static class DependencyInjectionExtension
 {
     public static void AdicionarInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        AdicionarFluentMigrator(services, configuration);
         AdicionarContexto(services, configuration);
-        AdicionarDbConnection(services, configuration);
         AdicionarRepositorios(services);
-    }
-
-    private static void AdicionarFluentMigrator(IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddFluentMigratorCore()
-        .ConfigureRunner(c => c.AddSqlServer()
-        .WithGlobalConnectionString(configuration.GetConexaoCompleta())
-        .ScanIn(Assembly.Load("SistemaDeCadastro.Infrastructure")).For.All());
     }
 
     private static void AdicionarContexto(IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConexaoCompleta();
+        var connectionString = configuration.GetConnectionString("Conexao");
 
         services.AddDbContext<SistemaDeCadastroContext>(opcoes =>
         {
             opcoes.UseSqlServer(connectionString);
-        });
-    }
-
-    private static void AdicionarDbConnection(IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddSingleton<IDbConnection>
-        (provider =>
-        {
-            var connection = new SqlConnection(configuration.GetConexaoCompleta());
-            connection.Open();
-            return connection;
         });
     }
 
