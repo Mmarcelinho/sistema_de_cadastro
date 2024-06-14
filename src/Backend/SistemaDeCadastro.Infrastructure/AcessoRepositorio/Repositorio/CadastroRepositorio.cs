565d91839e5dba9d38a72e06e9029cba3c6c3e22
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-14 15:14:46 -0300

    Altera métodos RecuperarPorId de consulta e atualização

--- src/Backend/SistemaDeCadastro.Infrastructure/AcessoRepositorio/Repositorio/CadastroRepositorio.cs
+++ src/Backend/SistemaDeCadastro.Infrastructure/AcessoRepositorio/Repositorio/CadastroRepositorio.cs
@@ -7,12 +7,17 @@ public class CadastroRepositorio : ICadastroReadOnlyRepositorio, ICadastroWriteO
     public CadastroRepositorio(SistemaDeCadastroContext contexto) => _contexto = contexto;
     public async Task<IEnumerable<Cadastro>> RecuperarTodos() => await _contexto.Cadastros.AsNoTracking().ToListAsync();
 
+    async Task<Cadastro> ICadastroReadOnlyRepositorio.RecuperarPorId(long cadastroId) => await _contexto.Cadastros
+    .AsNoTracking()
+    .FirstOrDefaultAsync(cadastro => cadastro.Id == cadastroId);
+
+    async Task<Cadastro> ICadastroUpdateOnlyRepositorio.RecuperarPorId(long cadastroId) => await _contexto.Cadastros.FirstOrDefaultAsync(cadastro => cadastro.Id == cadastroId);
 
     public async Task<bool> RecuperarCadastroExistentePorEmail(string email) => await _contexto.Cadastros.AnyAsync(cadastro => cadastro.Email.Equals(email));
 
-    async Task<Cadastro> ICadastroReadOnlyRepositorio.RecuperarPorId(long cadastroId) => await _contexto.Cadastros.AsNoTracking().FirstOrDefaultAsync(cadastro => cadastro.Id == cadastroId);
-
     public async Task Registrar(Cadastro cadastro) => await _contexto.Cadastros.AddAsync(cadastro);
+
+    public void Atualizar(Cadastro cadastro) => _contexto.Cadastros.Update(cadastro);
 
     public async Task<bool> Deletar(long cadastroId)
     {
@@ -25,8 +30,4 @@ public class CadastroRepositorio : ICadastroReadOnlyRepositorio, ICadastroWriteO
 
         return true;
     }
-
-    public void Atualizar(Cadastro cadastro) => _contexto.Cadastros.Update(cadastro);
-
-    async Task<Cadastro> ICadastroUpdateOnlyRepositorio.RecuperarPorId(long cadastroId) => await _contexto.Cadastros.AsNoTracking().FirstOrDefaultAsync(cadastro => cadastro.Id == cadastroId);
 }
