5454eab9dd9484a1d7857efcf55cfa72cc10b95a
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-14 21:42:02 -0300

    Adiciona global usings

--- src/Backend/SistemaDeCadastro.Application/Usings.cs
+++ src/Backend/SistemaDeCadastro.Application/Usings.cs
@@ -8,13 +8,17 @@ global using SistemaDeCadastro.Domain.Entidades;
 global using SistemaDeCadastro.Domain.Enum;
 global using SistemaDeCadastro.Domain.Repositorios;
 global using SistemaDeCadastro.Domain.Repositorios.Cadastro;
+global using SistemaDeCadastro.Domain.Repositorios.Pessoa;
 global using SistemaDeCadastro.Domain.ValueObjects;
 
 global using SistemaDeCadastro.Application.UseCases.Cadastro.Registrar;
 global using SistemaDeCadastro.Application.Servicos.CepServices;
+global using SistemaDeCadastro.Application.UseCases.Cadastro;
 
 global using SistemaDeCadastro.Comunicacao.Requisicoes.Cadastro;
 global using SistemaDeCadastro.Comunicacao.Respostas.Cadastro;
+global using SistemaDeCadastro.Comunicacao.Requisicoes.Pessoa;
+global using SistemaDeCadastro.Comunicacao.Respostas.Pessoa;
 
 global using SistemaDeCadastro.Exceptions;
 global using SistemaDeCadastro.Exceptions.ExceptionsBase;
