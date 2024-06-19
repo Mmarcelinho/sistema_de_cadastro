ea771669b43fc624ba67971fc95a21d75d59a785
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-19 01:05:48 -0300

    Adiciona global usings, pacotes e referências

---
+++ tests/UseCases.Test/Usings.cs
@@ -0,0 +1,11 @@
+global using FluentAssertions;
+
+global using SistemaDeCadastro.Application.UseCases.Cadastro.Registrar;
+
+global using SistemaDeCadastro.Exceptions;
+global using SistemaDeCadastro.Exceptions.ExceptionsBase;
+
+global using Utilitarios.Testes.Repositorios;
+global using Utilitarios.Testes.Repositorios.Cadastro;
+global using Utilitarios.Testes.Requisicoes.Cadastro;
+
