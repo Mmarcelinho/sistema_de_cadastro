4740fc9e296aac4eef8afd2e23f481f1f151ad26
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-19 23:48:11 -0300

    Adiciona global usings

---
+++ tests/WebApi.Test/Usings.cs
@@ -0,0 +1,9 @@
+global using System.Net.Http.Json;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using SistemaDeCadastro.Domain.Entidades;
+global using SistemaDeCadastro.Infrastructure.AcessoRepositorio;
+global using Utilitarios.Testes.Entidades;
+
