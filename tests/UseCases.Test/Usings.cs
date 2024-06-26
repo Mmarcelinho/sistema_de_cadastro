119a66e7420f6ec5c30676434712aa9b94d2497d
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-26 15:47:35 -0300

    Adiciona global usings

--- tests/UseCases.Test/Usings.cs
+++ tests/UseCases.Test/Usings.cs
@@ -5,12 +5,19 @@ global using SistemaDeCadastro.Application.UseCases.Pessoa.Registrar;
 global using SistemaDeCadastro.Exceptions;
 global using SistemaDeCadastro.Exceptions.ExceptionsBase;
 
+global using Utilitarios.Testes.Entidades;
 global using Utilitarios.Testes.Repositorios;
 global using Utilitarios.Testes.Repositorios.Cadastro;
 global using Utilitarios.Testes.Requisicoes.Cadastro;
 global using Utilitarios.Testes.Repositorios.Pessoa;
 global using Utilitarios.Testes.Requisicoes.Pessoa;
 global using Utilitarios.Testes.Servicos;
+
+global using SistemaDeCadastro.Application.UseCases.Cadastro.RecuperarTodos;
+global using SistemaDeCadastro.Application.UseCases.Cadastro.RecuperarPorId;
+
+global using SistemaDeCadastro.Application.UseCases.Pessoa.RecuperarTodos;
+global using SistemaDeCadastro.Application.UseCases.Pessoa.RecuperarPorId;
 
 
 
