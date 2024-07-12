d67fe5fcf7ec3f8ce7875d2e00aca6ae76fdd040
Mmarcelinho <marcelorosario2001@gmail.com>
2024-07-12 02:01:55 -0300

    Refatora value objects alterando de classes para records

--- src/Backend/SistemaDeCadastro.Domain/ValueObjects/Identificacao.cs
+++ src/Backend/SistemaDeCadastro.Domain/ValueObjects/Identificacao.cs
@@ -1,24 +1,3 @@
 namespace SistemaDeCadastro.Domain.ValueObjects;
 
-public class Identificacao : ValueObject
-{
-    public int Empresa { get; }
-
-    public string Identificador { get; }
-
-    public IdentificacaoTipo Tipo { get; }
-
-    public Identificacao(int empresa, string identificador, IdentificacaoTipo tipo)
-    {
-        Empresa = empresa;
-        Identificador = identificador;
-        Tipo = tipo;
-    }
-
-    protected override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Empresa;
-        yield return Identificador;
-        yield return Tipo;
-    }
-}
+public record Identificacao(int Empresa, string Identificador, IdentificacaoTipo Tipo);
